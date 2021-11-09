b621ecc5888cdf1375d8fe173955b11c4b45896c
WORK-PC <WORK-PC@DESKTOP-K647S7J>
2021-11-09 17:55:55 -0500

    Version 0

---
+++ src/cv_ejercicio/Cv_ejercicio.java
@@ -0,0 +1,18 @@
+
+package cv_ejercicio;
+
+/**
+ *
+ * @author Alejandro Jaimes
+ */
+public class Cv_ejercicio {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Version 0");
+    }
+    
+}
